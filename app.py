36fb86bb794ca5ebeaf6eceb0700838affcd0644
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-20 00:05:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import streamlit as st
 import openai
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.units import inch
-from reportlab.platypus import SimpleDocTemplate, Paragraph
-from reportlab.lib.enums import TA_JUSTIFY
-from reportlab.lib.styles import ParagraphStyle
-from reportlab.platypus import Spacer
-
 
 # Set up the OpenAI API client
 openai.api_key = st.secrets["openai_api_key"]
@@ -16,38 +8,19 @@ openai.api_key = st.secrets["openai_api_key"]
 st.title("Mini Project Report Generator")
 project_name = st.text_input("Enter a project name:")
 
-# Generate report using OpenAI API
-if st.button("Generate Report"):
-    prompt = f"Generate a mini project report for the project {project_name} with the following sections: Introduction, Literature Review, Methodology, Results, Discussion, Conclusion, References, Acknowledgements, and Appendix."
+def generate_intro(project_name):
+    intro_prompt = f"Write an introduction for the project '{project_name}'."
     response = openai.Completion.create(
-      engine="text-davinci-002",
-      prompt=prompt,
-      temperature=0.8,
-      max_tokens=2048,
-      n=1,
-      stop=None,
-      timeout=60,
+        engine="text-davinci-002",
+        prompt=intro_prompt,
+        max_tokens=4096,
+        n=1,
+        stop=None,
+        temperature=0.7,
     )
-    report_text = response.choices[0].text
+    return response.choices[0].text
 
-    # Create PDF file using reportlab
-    doc = SimpleDocTemplate("mini_project_report.pdf", pagesize=letter,
-                            rightMargin=72, leftMargin=72,
-                            topMargin=72, bottomMargin=18)
-    styles = getSampleStyleSheet()
-    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
-    report = []
-    for line in report_text.split('\n'):
-        report.append(Paragraph(line, styles["Justify"]))
-        report.append(Spacer(1, 12))
-    doc.build(report)
-
-    # Create download button
-    with open("mini_project_report.pdf", "rb") as f:
-        bytes_data = f.read()
-        st.download_button(
-            label="Download Report",
-            data=bytes_data,
-            file_name="mini_project_report.pdf",
-            mime="application/pdf",
-        )
+if project_name:
+    intro = generate_intro(project_name)
+    st.header("Introduction")
+    st.write(intro)
