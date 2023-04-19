88814be89c5c78ddc1f03802da72c271ff107d9d
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-20 00:35:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("Mini Project Report Generator")
 project_name = st.text_input("Enter a project name:")
 
 def generate_intro(project_name):
-    intro_prompt = f"Write an introduction for the project in more than 1000 words'{project_name}'."
+    intro_prompt = f"Write an introduction for the project'{project_name}'."
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=intro_prompt,
@@ -20,16 +20,25 @@ def generate_intro(project_name):
     )
     return response.choices[0].text + response.choices[1].text + response.choices[2].text + response.choices[3].text + response.choices[4].text + response.choices[5].text + response.choices[6].text + response.choices[7].text + response.choices[8].text + response.choices[9].text
 
-
 if project_name:
     intro = generate_intro(project_name)
-    st.header("Introduction")
+    st.header("Introduction")    
+    st.write(intro)
     
-    #st.write(f"<p style='text-align: justify'>{intro}</p>", unsafe_allow_html=True)
     
-    # Apply CSS styling to align text to both left and right margin
-    styled_text = f"<p style='text-align: justify;'>{intro}</p>"
-    # Display the styled text using st.markdown
-    st.markdown(styled_text, unsafe_allow_html=True)
-    
-    #st.write(intro)
+    def generate_lit_survey(project_name):
+    intro_prompt = f"Write an Write a literature survey for the project '{project_name}'."
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=intro_prompt,
+        max_tokens=2048,
+        n=10,
+        stop=None,
+        temperature=0.7,
+    )
+    return response.choices[0].text + response.choices[1].text + response.choices[2].text + response.choices[3].text + response.choices[4].text + response.choices[5].text + response.choices[6].text + response.choices[7].text + response.choices[8].text + response.choices[9].text
+
+if project_name:
+    LS = generate_lit_survey(project_name)
+    st.header("Literature Survey")    
+    st.write(LS)
