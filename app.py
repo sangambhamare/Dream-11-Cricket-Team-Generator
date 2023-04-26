c4192a406308ee3f4714a68e225a2a364a2f1a8d
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-26 22:03:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,3 +34,19 @@ if uploaded_file is not None:
 
     # Display the original and cartoonified image
     st.image([image, cartoon], caption=["Original Image", "Cartoonified Image"], width=500)
+    
+    
+def cartoonify_image(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 5)
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    color = cv2.bilateralFilter(image, 9, 250, 250)
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+    return cartoon
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    cartoon = cartoonify_image(image)
+    st.image([image, cartoon], caption=["Original Image", "Cartoonified Image"], width=500)    
