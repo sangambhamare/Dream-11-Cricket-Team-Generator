e9b7c0250b69941cb05c55b4045d982cb72a24ff
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-26 22:32:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,36 @@ import streamlit as st
 import numpy as np
 import cv2
 
-st.title("Cartoonify Your Image")
+st.title("Clean your Image")
         
-def cartoonify_image(uploaded_file):
-    gray = cv2.cvtColor(uploaded_file, cv2.COLOR_BGR2GRAY)
-    gray = cv2.medianBlur(gray, 5)
-    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 5)
-    color = cv2.bilateralFilter(image, 9, 30, 30)
-    cartoon = cv2.bitwise_and(color, color, mask=edges)
-    return cartoon
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+# Upload an image file
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
+
 if uploaded_file is not None:
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    cartoon = cartoonify_image(image)
-    st.image([cartoon], caption=["Cartoonified Image"], width=400)
+    # Load the uploaded image
+    image = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    
+    # Apply bilateral filter to reduce noise and preserve edges
+    color = cv2.bilateralFilter(image, 9, 30, 250)
+    
+    # Convert the filtered image to grayscale
+    gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+    
+    # Apply histogram equalization to improve contrast
+    equ = cv2.equalizeHist(gray)
+    
+    # Apply unsharp masking to enhance edges and details
+    blur = cv2.GaussianBlur(equ, (5, 5), 0)
+    alpha = 1.5
+    beta = -0.5
+    usm = cv2.addWeighted(blur, alpha, equ, beta, 0)
+    
+    # Apply adaptive thresholding to make teeth white and hair clear
+    th = cv2.adaptiveThreshold(usm, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    
+    # Display the original and processed images
+    st.image(np.hstack((image, th)), channels="BGR")
 
