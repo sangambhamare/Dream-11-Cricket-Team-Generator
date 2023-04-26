0ac30a8d7baa2217abfc184cd2713c50148b0512
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-26 22:21:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def cartoonify_image(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 5)
     edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
-    color = cv2.bilateralFilter(image, 9, 30, 250)
+    color = cv2.bilateralFilter(image, 9, 30, 30)
     cartoon = cv2.bitwise_and(color, color, mask=edges)
     return cartoon
 
