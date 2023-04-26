5ac30dd6764e5af9ce53121f38cce465a69c96e4
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-26 22:42:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import cv2
 
 st.title("Clean your Image")
         
-
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # Upload an image file
@@ -15,29 +14,19 @@ if uploaded_file is not None:
     image = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-    # Apply bilateral filter to reduce noise and preserve edges
-    color = cv2.bilateralFilter(image, 9, 30, 250)
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Convert the filtered image to grayscale
-    gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+    # Apply Canny edge detection algorithm to extract the outlines
+    edges = cv2.Canny(gray, 100, 200)
 
-    # Apply histogram equalization to improve contrast
-    equ = cv2.equalizeHist(gray)
+    # Convert the edges to 3 channels
+    edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
 
-    # Apply unsharp masking to enhance edges and details
-    blur = cv2.GaussianBlur(equ, (5, 5), 0)
-    alpha = 1.5
-    beta = -0.5
-    usm = cv2.addWeighted(blur, alpha, equ, beta, 0)
-
-    # Apply adaptive thresholding to make teeth white and hair clear
-    th = cv2.adaptiveThreshold(usm, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    th = cv2.cvtColor(th, cv2.COLOR_GRAY2BGR)  # Convert to 3 channels
-
-    # Display the original and processed images
+    # Display the original and outline images
     col1, col2 = st.columns(2)
     col1.subheader("Original Image")
     col1.image(image, channels="BGR")
 
-    col2.subheader("Processed Image")
-    col2.image(th, channels="BGR")
+    col2.subheader("Outline Image")
+    col2.image(edges, channels="BGR")
