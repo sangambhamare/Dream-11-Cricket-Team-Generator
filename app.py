3c3dcbc06b0ed433e1f2635c1d8fb54dc6a02b66
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-26 22:55:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,24 +17,33 @@ if uploaded_file is not None:
     # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Apply Canny edge detection algorithm to extract the outlines
-    edges = cv2.Canny(gray, 100, 200)
+    # Apply bilateral filter to smooth the grayscale image
+    smooth = cv2.bilateralFilter(gray, 9, 75, 75)
 
-    # Create a white background image
-    white = np.zeros_like(image)
-    white.fill(255)
+    # Extract edges from the smoothed image using Canny edge detection algorithm
+    edges = cv2.Canny(smooth, 100, 200)
 
-    # Invert the edges and convert to 3 channels
-    edges = cv2.bitwise_not(edges)
-    edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+    # Create a mask by applying a threshold to the edges
+    mask = cv2.threshold(edges, 50, 255, cv2.THRESH_BINARY_INV)[1]
 
-    # Copy the original image onto the white background, using the edges as a mask
-    result = np.where(edges != 0, edges, white)
-    result = cv2.bitwise_and(image, result)
+    # Apply a bilateral filter to the original image to reduce noise and preserve edges
+    cartoon = cv2.bilateralFilter(image, 9, 250, 250)
 
-    # Display the original and outline images
+    # Apply the mask to the cartoon image to reveal only the edges
+    cartoon = cv2.bitwise_and(cartoon, cartoon, mask=mask)
+
+    # Display the original image, grayscale image, smoothed image, edges, and cartoon image
     st.subheader("Original Image")
     st.image(image, channels="BGR")
 
-    st.subheader("Outline Image")
-    st.image(result, channels="BGR")
+    st.subheader("Grayscale Image")
+    st.image(gray, channels="GRAY")
+
+    st.subheader("Smoothed Image")
+    st.image(smooth, channels="GRAY")
+
+    st.subheader("Edges")
+    st.image(edges, channels="GRAY")
+
+    st.subheader("Cartoon Image")
+    st.image(cartoon, channels="BGR")
