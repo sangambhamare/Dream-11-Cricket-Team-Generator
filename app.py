f6272e10798c5915dbfa9987c4d780e255bb8557
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-26 23:20:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,17 +24,10 @@ if uploaded_file is not None:
     # Prepare mask of the image
     color = cv2.bilateralFilter(image, 9, 250, 250)
     mask = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-
-    # Add a black border to the image
-    border_size = 10
-    border_color = [0, 0, 0]
-    image = cv2.copyMakeBorder(image, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=border_color)
-
-    # Apply the cartoon effect
     output = cv2.bitwise_and(color, mask)
 
     # Display the original and processed images
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     with col1:
         st.write("Original Image")
         st.image(image, channels="BGR")
@@ -42,13 +35,5 @@ if uploaded_file is not None:
     with col2:
         st.write("Cartoon Image")
         st.image(output, channels="BGR")
-
-    with col3:
-        st.write("Colored Cartoon Image")
-        output_gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-        _, mask = cv2.threshold(output_gray, 10, 255, cv2.THRESH_BINARY)
-        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        color_output = cv2.bitwise_and(image, mask)
-        st.image(color_output, channels="BGR")
         
     st.code("© Developed by Mr. Sangam Bhamare & SQUAD. All rights reserved.")
