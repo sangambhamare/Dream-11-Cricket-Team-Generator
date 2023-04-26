5659bddc24c177c9bc2236882b93c540c799efa5
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2023-04-26 22:34:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,25 +13,25 @@ if uploaded_file is not None:
     # Load the uploaded image
     image = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    
+
     # Apply bilateral filter to reduce noise and preserve edges
     color = cv2.bilateralFilter(image, 9, 30, 250)
-    
+
     # Convert the filtered image to grayscale
     gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-    
+
     # Apply histogram equalization to improve contrast
     equ = cv2.equalizeHist(gray)
-    
+
     # Apply unsharp masking to enhance edges and details
     blur = cv2.GaussianBlur(equ, (5, 5), 0)
     alpha = 1.5
     beta = -0.5
     usm = cv2.addWeighted(blur, alpha, equ, beta, 0)
-    
+
     # Apply adaptive thresholding to make teeth white and hair clear
     th = cv2.adaptiveThreshold(usm, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    
+    th = cv2.cvtColor(th, cv2.COLOR_GRAY2BGR)  # Convert to 3 channels
+
     # Display the original and processed images
     st.image(np.hstack((image, th)), channels="BGR")
-
